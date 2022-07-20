3c1472678c602b54d7977c8744bb5633bb12b19a
조은채 <lunch0706@naver.com>
2022-07-20 16:18:05 +0900

    error 상황

--- config/settings/base.py
+++ config/settings/base.py
@@ -128,63 +128,63 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-# 로깅 설정
+# 로깅설정
 LOGGING = {
-    'version':1,
-    'disable_existing_loggers':False,
-    'filters':{
-        'require_debug_false':{
-            '()':'django.utils.log.RequireDebugFalse'
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
         },
-        'require_debug_true':{
-            '()':'django.utils.log.RequireDebugTrue'
-        },
-    },
-    'formatters':{
-        'django.server':{
-            '()':'django.utils.log.ServerFormatter',
-            'format':'[{server_time}] {message}',
-            'style':'{'
-        }
-    },
-    'handlers':{
-        'console':{
-            'level':'INFO',
-            'filters':['require_debug_true'],
-            'class':'logging.StreamHandler'
-        },
-        'django.server':{
-            'level':'INFO',
-            'class':'logging.StreanHandler',
-            'formatter':'django.server'
-        },
-        'standard':{
-            'format':'%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        },
-        'mail_admins':{
-            'level':'ERROR',
-            'filters':['require_debug_false'],
-            'class':'django.utils.log.AdminEmailHandler'
-        },
-        'file':{
-            'level':'INFO',
-            'filters':['require_debug_false'], # DEBUG=False 운영 환경에서 사용
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename':BASE_DIR / 'logs/mysite.log', # 로그 파일명은 logs 디렉터리에 mysite.log로 설정
-            'maxBytes':1024*1024*5, # 5 MB
-            'backupCount':5,
-            'formatter':'standard'
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
         },
     },
-    'loggers':{
-        'django':{ # 장고 로거
-            'handlers':['console', 'mail_admins','file'],
-            'level':'INFO'
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[{server_time}] {message}',
+            'style': '{',
         },
-        'django.server':{
-            'handlers':['django.server'],
-            'level':'INFO',
-            'propagate':False
-        }
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'file': {
+            'level': 'INFO',
+            'filters': ['require_debug_false'],
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': BASE_DIR / 'logs/mysite.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'mail_admins', 'file'],
+            'level': 'INFO',
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
+            'propagate': False,
+        },
     }
 }
