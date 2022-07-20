ef19755447bedf5bad64500a680ff90a6917271d
조은채 <lunch0706@naver.com>
2022-07-20 11:54:50 +0900

    DEBUG 설정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,4 @@ from .base import *
 ALLOWED_HOSTS = ['13.209.252.184']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = [] # STATICFILES_DIRS의 리스트에 STATIC_ROOT와 동일한 디렉터리 포함되어 있으면 오류나는 걸 방지
+DEBUG = False
